d91595dc982495e08c7ce9a6adb4d01795c17d91
Sergej Samsonenko <contact@sergej.codes>
2021-12-14 19:38:20 +0100

    chore: add comments

--- packages/minze/lib/minze-element.ts
+++ packages/minze/lib/minze-element.ts
@@ -4,10 +4,17 @@ export class MinzeElement extends HTMLElement {
     this.attachShadow({ mode: 'open' })
   }
 
+  /**
+   * Run the render method whenever the element
+   * is appended into a document-connected element
+   */
   connectedCallback() {
     this.render()
   }
 
+  /**
+   * Render html and css inside the shadow dom
+   */
   render() {
     if (this.shadowRoot) {
       this.shadowRoot.innerHTML = `
@@ -17,10 +24,20 @@ export class MinzeElement extends HTMLElement {
     }
   }
 
+  /**
+   * HTML
+   * Return regular HTML for the render method
+   * as a string or template literal
+   */
   html() {
     return '<slot></slot>'
   }
 
+  /**
+   * CSS
+   * Return regular CSS for the render method
+   * as a string or template literal
+   */
   css() {
     return ''
   }
