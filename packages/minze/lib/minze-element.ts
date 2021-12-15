d4ba4e7e7a37e43d3b64dc112f56c683d14f7981
Sergej Samsonenko <contact@sergej.codes>
2021-12-15 23:17:13 +0100

    refactor: minze-element

--- packages/minze/lib/minze-element.ts
+++ packages/minze/lib/minze-element.ts
@@ -1,44 +1,82 @@
+/**
+ * Types
+ */
+export type minzeEvent = [part: string, type: string, fn: () => void]
+
+/**
+ * Base MinzeElement class
+ */
 export class MinzeElement extends HTMLElement {
+  data?: Record<string, unknown>
+  events?: minzeEvent[]
+  html?(): string
+  css?(): string
+
   constructor() {
     super()
     this.attachShadow({ mode: 'open' })
   }
 
   /**
-   * Run the render method whenever the element
-   * is appended into a document-connected element
+   * Runs whenever the element is appended
+   * into a document-connected element
    */
   connectedCallback() {
     this.render()
   }
 
   /**
-   * Render html and css inside the shadow dom
+   * Runs each time the element is disconnected
+   * from the document's DOM
+   */
+  disconnectedCallback() {
+    this.registerEvents('remove')
+  }
+
+  /**
+   * Renders the template into the shadow DOM
+   * and attaches all event listeners
    */
   render() {
     if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = `
-        ${this.css || this.css() !== '' ? `<style>${this.css()}</style>` : ''}
-        ${this.html()}
-      `
+      this.shadowRoot.innerHTML = this.template()
+      this.registerEvents('add')
     }
   }
 
   /**
-   * HTML
-   * Return regular HTML for the render method
-   * as a string or template literal
+   * Creates the template for the shadow root
    */
-  html() {
-    return '<slot></slot>'
+  template() {
+    return `
+      ${this.css && this.css() !== '' ? `<style>${this.css()}</style>` : ''}
+      ${(this.html && this.html()) || '<slot></slot>'}
+    `
   }
 
   /**
-   * CSS
-   * Return regular CSS for the render method
-   * as a string or template literal
+   * Adds or removes all event listeners
+   * inside the events property
    */
-  css() {
-    return ''
+  private registerEvents(action: 'add' | 'remove') {
+    if (this.events?.length) {
+      this.events.forEach((eventTuple) => {
+        const [part, type, fn] = eventTuple
+
+        if (
+          typeof part === 'string' &&
+          typeof type === 'string' &&
+          typeof fn === 'function'
+        ) {
+          const parts = this.shadowRoot?.querySelectorAll(`[part='${part}']`)
+
+          parts?.forEach((part) => {
+            action === 'add'
+              ? part.addEventListener(type, fn)
+              : part.removeEventListener(type, fn)
+          })
+        }
+      })
+    }
   }
 }
