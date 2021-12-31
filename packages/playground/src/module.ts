1d6f56354a30f006a225606ad2acc9d9a4d70790
Sergej Samsonenko <contact@sergej.codes>
2021-12-31 17:24:37 +0100

    refactor: auto import all elements

--- packages/playground/src/module.ts
+++ packages/playground/src/module.ts
@@ -1,8 +1,1 @@
-import Minze from 'minze'
-import { PlaygroundElement } from './lib/playground-element'
-
-Minze.defineAll([PlaygroundElement])
-
-export const template = `
-  <playground-element></playground-element>
-`
+export * from './lib/playground-element'
--- packages/playground/src/vite.ts
+++ packages/playground/src/vite.ts
@@ -1,5 +1,9 @@
 import './assets/vite.css'
-import { template } from './module'
+import Minze from 'minze'
+import * as MinzeElements from './module'
+import template from './template'
+
+Minze.defineAll(Object.values(MinzeElements))
 
 const app = document.querySelector<HTMLDivElement>('#app') ?? null
 if (app) app.innerHTML = template
