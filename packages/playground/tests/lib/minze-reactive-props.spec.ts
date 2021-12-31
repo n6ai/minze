94f36031ac5afcfea8c9b6fab2bcd19ab42823c9
Sergej Samsonenko <contact@sergej.codes>
2021-12-31 19:03:34 +0100

    test: check for nested objects

--- packages/playground/tests/lib/minze-reactive-props.spec.ts
+++ packages/playground/tests/lib/minze-reactive-props.spec.ts
@@ -8,12 +8,15 @@ test(element, async ({ page }) => {
   await start(page, template)
 
   const selector = `${element} button`
+  const complexSelector = `${element} div`
 
   expect(await page.locator(selector).innerText()).toBe('0')
   expect(await page.getAttribute(element, 'count')).toBe('0')
+  expect(await page.locator(complexSelector).innerText()).toBe('nested')
 
   await page.click(selector)
 
   expect(await page.locator(selector).innerText()).toBe('1')
   expect(await page.getAttribute(element, 'count')).toBe('1')
+  expect(await page.locator(complexSelector).innerText()).toBe('changed')
 })
--- packages/playground/tests/lib/minze-reactive-props.ts
+++ packages/playground/tests/lib/minze-reactive-props.ts
@@ -2,19 +2,43 @@ import { MinzeElement, MinzeProps, MinzeEvents } from 'minze'
 
 export interface MinzeReactiveProps {
   count: number
+  complex: {
+    deep: {
+      nested: {
+        value: string
+      }
+    }
+  }
 }
 
 export class MinzeReactiveProps extends MinzeElement {
-  reactive: MinzeProps = [['count', 0, true]]
+  reactive: MinzeProps = [
+    ['count', 0, true],
+    [
+      'complex',
+      {
+        deep: {
+          nested: {
+            value: 'nested'
+          }
+        }
+      }
+    ]
+  ]
 
   html = () => `
     <button>
       ${this.count}
     </button>
+
+    <div>
+      ${this.complex.deep.nested.value}
+    </div>
   `
 
   handleClick = () => {
     this.count++
+    this.complex.deep.nested.value = 'changed'
   }
 
   eventListeners: MinzeEvents = [['button', 'click', this.handleClick]]
