45914314627e475d7a28d5b6fd33f8cc64e455dc
Sergej Samsonenko <contact@sergej.codes>
2022-01-02 12:13:29 +0100

    test: add case for watchers

---
+++ packages/playground/tests/minze-element/lib/minze-reactive-watchers.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test'
+import { start } from 'playground/tests/minze-element/utils'
+
+const element = 'minze-reactive-watchers'
+
+test(`MinzeElement: ${element}`, async ({ page }) => {
+  const template = `<${element}></${element}>`
+  await start(page, template)
+
+  const selectorStr = `${element} .str`
+  const selectorArr = `${element} .arr`
+  const selectorObj = `${element} .obj`
+
+  expect(await page.locator(selectorStr).innerText()).toBe('')
+  expect(await page.locator(selectorArr).innerText()).toBe('')
+  expect(await page.locator(selectorObj).innerText()).toBe('')
+
+  await page.click(`${element} button`)
+
+  expect(await page.locator(selectorStr).innerText()).toBe(
+    'changed text, initial text'
+  )
+  expect(await page.locator(selectorArr).innerText()).toBe('4, undefined')
+  expect(await page.locator(selectorObj).innerText()).toBe(
+    'changed value, initial value'
+  )
+})
