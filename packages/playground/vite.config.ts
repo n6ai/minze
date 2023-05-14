ce5cdd0a7fa6b3ffff5e33a83e2e3e65c5add3f7
Sergej Samsonenko <contact@sergej.codes>
2023-05-14 14:51:06 +0200

    chore: use new ts template for playground

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,3 +1,31 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
-export default defineConfig({})
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const modes = ['module', 'cdn']
+
+  if (command === 'build' && !modes.includes(mode)) {
+    console.error('mode must be one of: ' + modes.join(', '))
+    process.exit(1)
+  }
+
+  const isModule = mode === 'module'
+
+  return {
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        keep_classnames: true
+      },
+      emptyOutDir: isModule,
+      lib: {
+        name: 'minze',
+        formats: [isModule ? 'es' : 'umd'],
+        entry: isModule ? 'src/module.ts' : 'src/cdn.ts',
+        fileName: () => (isModule ? 'module.js' : 'cdn.js')
+      }
+    },
+    plugins: [isModule ? dts({ rollupTypes: true }) : null]
+  }
+})
