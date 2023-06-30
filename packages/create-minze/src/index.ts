13c3d84d1e71fcbcea7fa784e94c261e81318edc
Sergej Samsonenko <contact@sergej.codes>
2023-06-30 19:39:57 +0200

    refactor: replace js with ts code

---
+++ packages/create-minze/src/index.ts
@@ -0,0 +1,264 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import minimist from 'minimist'
+import prompts from 'prompts'
+import { blue, red, reset, yellow } from 'kolorist'
+
+// Avoids autoconversion to number of the project name by defining that the args
+// non associated with an option ( _ ) needs to be parsed as a string. See #4606
+const argv = minimist<{
+  t?: string
+  template?: string
+}>(process.argv.slice(2), { string: ['_'] })
+
+const cwd = process.cwd()
+
+type ColorFunc = (str: string | number) => string
+
+type TemplateVariant = {
+  name: string
+  display: string
+  color: ColorFunc
+}
+
+const TEMPLATES: TemplateVariant[] = [
+  {
+    name: 'js',
+    display: 'JavaScript',
+    color: yellow
+  },
+  {
+    name: 'ts',
+    display: 'TypeScript',
+    color: blue
+  }
+]
+
+const TEMPLATE_NAMES = TEMPLATES.map((f) => f.name)
+
+const renameFiles: Record<string, string | undefined> = {
+  _gitignore: '.gitignore'
+}
+
+const defaultTargetDir = 'minze-project'
+
+async function init() {
+  const argTargetDir = formatTargetDir(argv._[0])
+  const argTemplate = argv.template || argv.t
+
+  let targetDir = argTargetDir || defaultTargetDir
+  const getProjectName = () =>
+    targetDir === '.' ? path.basename(path.resolve()) : targetDir
+
+  let result: prompts.Answers<
+    'projectName' | 'overwrite' | 'packageName' | 'template'
+  >
+
+  try {
+    result = await prompts(
+      [
+        {
+          type: argTargetDir ? null : 'text',
+          name: 'projectName',
+          message: reset('Project name:'),
+          initial: defaultTargetDir,
+          onState: (state) => {
+            targetDir = formatTargetDir(state.value) || defaultTargetDir
+          }
+        },
+        {
+          type: () =>
+            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : 'confirm',
+          name: 'overwrite',
+          message: () =>
+            (targetDir === '.'
+              ? 'Current directory'
+              : `Target directory "${targetDir}"`) +
+            ` is not empty. Remove existing files and continue?`
+        },
+        {
+          type: (_, { overwrite }: { overwrite?: boolean }) => {
+            if (overwrite === false) {
+              throw new Error(red('✖') + ' Operation cancelled')
+            }
+            return null
+          },
+          name: 'overwriteChecker'
+        },
+        {
+          type: () => (isValidPackageName(getProjectName()) ? null : 'text'),
+          name: 'packageName',
+          message: reset('Package name:'),
+          initial: () => toValidPackageName(getProjectName()),
+          validate: (dir) =>
+            isValidPackageName(dir) || 'Invalid package.json name'
+        },
+        {
+          type:
+            argTemplate && TEMPLATE_NAMES.includes(argTemplate)
+              ? null
+              : 'select',
+          name: 'template',
+          message:
+            typeof argTemplate === 'string' &&
+            !TEMPLATE_NAMES.includes(argTemplate)
+              ? `"${argTemplate}" isn't a valid template. Please choose from below: `
+              : 'Select a template:',
+          initial: 0,
+          choices: TEMPLATES.map((template) => {
+            const templateColor = template.color
+            return {
+              title: templateColor(template.display),
+              value: template
+            }
+          })
+        }
+      ],
+      {
+        onCancel: () => {
+          throw new Error(red('✖') + ' Operation cancelled')
+        }
+      }
+    )
+  } catch (cancelled: any) {
+    console.log(cancelled.message)
+    return
+  }
+
+  // user choice associated with prompts
+  const { template, overwrite, packageName } = result
+
+  const root = path.join(cwd, targetDir)
+
+  if (overwrite) {
+    emptyDir(root)
+  } else if (!fs.existsSync(root)) {
+    fs.mkdirSync(root, { recursive: true })
+  }
+
+  // determine template
+  // let template: string = variant || framework?.name || argTemplate
+
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent)
+  const pkgManager = pkgInfo ? pkgInfo.name : 'npm'
+
+  console.log(`\nScaffolding project in ${root}...`)
+
+  const templateDir = path.resolve(
+    fileURLToPath(import.meta.url),
+    '../..',
+    `template-${template?.name ?? argTemplate}`
+  )
+
+  const write = (file: string, content?: string) => {
+    const targetPath = path.join(root, renameFiles[file] ?? file)
+    if (content) {
+      fs.writeFileSync(targetPath, content)
+    } else {
+      copy(path.join(templateDir, file), targetPath)
+    }
+  }
+
+  const files = fs.readdirSync(templateDir)
+  for (const file of files.filter((f) => f !== 'package.json')) {
+    write(file)
+  }
+
+  const pkg = JSON.parse(
+    fs.readFileSync(path.join(templateDir, `package.json`), 'utf-8')
+  )
+
+  pkg.name = packageName || getProjectName()
+
+  write('package.json', JSON.stringify(pkg, null, 2) + '\n')
+
+  const cdProjectName = path.relative(cwd, root)
+  console.log(`\nDone. Now run:\n`)
+  if (root !== cwd) {
+    console.log(
+      `  cd ${
+        cdProjectName.includes(' ') ? `"${cdProjectName}"` : cdProjectName
+      }`
+    )
+  }
+  switch (pkgManager) {
+    case 'yarn':
+      console.log('  yarn')
+      console.log('  yarn dev')
+      break
+    default:
+      console.log(`  ${pkgManager} install`)
+      console.log(`  ${pkgManager} run dev`)
+      break
+  }
+  console.log()
+}
+
+function formatTargetDir(targetDir: string | undefined) {
+  return targetDir?.trim().replace(/\/+$/g, '')
+}
+
+function copy(src: string, dest: string) {
+  const stat = fs.statSync(src)
+  if (stat.isDirectory()) {
+    copyDir(src, dest)
+  } else {
+    fs.copyFileSync(src, dest)
+  }
+}
+
+function isValidPackageName(projectName: string) {
+  return /^(?:@[a-z\d\-*~][a-z\d\-*._~]*\/)?[a-z\d\-~][a-z\d\-._~]*$/.test(
+    projectName
+  )
+}
+
+function toValidPackageName(projectName: string) {
+  return projectName
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/^[._]/, '')
+    .replace(/[^a-z\d\-~]+/g, '-')
+}
+
+function copyDir(srcDir: string, destDir: string) {
+  fs.mkdirSync(destDir, { recursive: true })
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file)
+    const destFile = path.resolve(destDir, file)
+    copy(srcFile, destFile)
+  }
+}
+
+function isEmpty(path: string) {
+  const files = fs.readdirSync(path)
+  return files.length === 0 || (files.length === 1 && files[0] === '.git')
+}
+
+function emptyDir(dir: string) {
+  if (!fs.existsSync(dir)) {
+    return
+  }
+  for (const file of fs.readdirSync(dir)) {
+    if (file === '.git') {
+      continue
+    }
+    fs.rmSync(path.resolve(dir, file), { recursive: true, force: true })
+  }
+}
+
+function pkgFromUserAgent(userAgent: string | undefined) {
+  if (!userAgent) return undefined
+  const pkgSpec = userAgent.split(' ')[0]
+  const pkgSpecArr = pkgSpec.split('/')
+  return {
+    name: pkgSpecArr[0],
+    version: pkgSpecArr[1]
+  }
+}
+
+init().catch((e) => {
+  console.error(e)
+})
