95cc405cc8bcb73ba0b404f200796458d9a0443e
Sergej Samsonenko <contact@sergej.codes>
2023-07-01 20:00:48 +0200

    fix: add plugin options for providing entry paths

--- packages/vite-plugin-minze/src/main.ts
+++ packages/vite-plugin-minze/src/main.ts
@@ -1,51 +1,60 @@
-import type { UserConfig } from 'vite'
+import type { Plugin, UserConfig } from 'vite'
 
-export default () => ({
-  name: 'vite-plugin-minze',
-  config: (
-    config: unknown,
-    { command, mode }: { command: 'build' | 'serve'; mode: string }
-  ): UserConfig => {
-    const modes = ['module', 'cdn']
+interface PluginOptions {
+  entry: {
+    module: string
+    cdn: string
+  }
+}
 
-    if (command === 'build' && !modes.includes(mode)) {
-      console.error(`mode must be one of: ${modes.join(', ')}`)
-      process.exit(1)
-    }
+export default (options: PluginOptions): Plugin => {
+  return {
+    name: 'vite-plugin-minze',
+    config: (
+      config,
+      { command, mode }: { command: 'build' | 'serve'; mode: string }
+    ): UserConfig => {
+      const modes = ['module', 'cdn']
 
-    const isModule = mode === 'module'
+      if (command === 'build' && !modes.includes(mode)) {
+        console.error(`mode must be one of: ${modes.join(', ')}`)
+        process.exit(1)
+      }
 
-    return {
-      build: {
-        minify: 'terser',
-        terserOptions: {
-          keep_classnames: true
-        },
-        emptyOutDir: isModule,
-        lib: {
-          name: 'minze',
-          formats: [isModule ? 'es' : 'umd'],
-          entry: isModule ? 'src/module.ts' : 'src/cdn.ts',
-          fileName: () => (isModule ? 'module.js' : 'cdn.js')
-        },
-        rollupOptions: {
-          output: {
-            inlineDynamicImports: !isModule,
-            minifyInternalExports: false,
-            chunkFileNames: '[name].js',
-            manualChunks: isModule
-              ? (id) => {
-                  if (id.includes('lib')) {
-                    const name = id.match(/(?<=lib\/).*(?=\.(ts|js))/i)?.[0]
-                    return `lib/${name}`
-                  } else if (id.match(/node_modules|minze\/dist/i)) {
-                    return 'vendor'
+      const isModule = mode === 'module'
+
+      return {
+        build: {
+          minify: 'terser',
+          terserOptions: {
+            keep_classnames: true
+          },
+          emptyOutDir: isModule,
+          lib: {
+            name: 'minze',
+            formats: [isModule ? 'es' : 'umd'],
+            entry: isModule ? options.entry.module : options.entry.cdn,
+            fileName: () => (isModule ? 'module.js' : 'cdn.js')
+          },
+          rollupOptions: {
+            output: {
+              inlineDynamicImports: !isModule,
+              minifyInternalExports: false,
+              chunkFileNames: '[name].js',
+              manualChunks: isModule
+                ? (id) => {
+                    if (id.includes('lib')) {
+                      const name = id.match(/(?<=lib\/).*(?=\.(ts|js))/i)?.[0]
+                      return `lib/${name}`
+                    } else if (id.match(/node_modules|minze\/dist/i)) {
+                      return 'vendor'
+                    }
                   }
-                }
-              : undefined
+                : undefined
+            }
           }
         }
       }
     }
   }
-})
+}
