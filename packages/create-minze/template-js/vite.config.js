b5deee77f16c3144e88758b4add34fb14ae90c0c
Sergej Samsonenko <contact@sergej.codes>
2023-07-01 20:01:15 +0200

    chore: update templates

--- packages/create-minze/template-js/vite.config.js
+++ packages/create-minze/template-js/vite.config.js
@@ -3,5 +3,5 @@ import minze from '@minzejs/vite-plugin-minze'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [minze()]
+  plugins: [minze({ entry: { module: 'src/module.js', cdn: 'src/cdn.js' } })]
 })
--- packages/create-minze/template-ts/vite.config.ts
+++ packages/create-minze/template-ts/vite.config.ts
@@ -4,5 +4,8 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [minze(), dts({ rollupTypes: true })]
+  plugins: [
+    minze({ entry: { module: 'src/module.ts', cdn: 'src/cdn.ts' } }),
+    dts({ rollupTypes: true })
+  ]
 })
