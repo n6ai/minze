b4e21297d6abd69ba16278cca9ae38c057e28af4
Sergej Samsonenko <contact@sergej.codes>
2023-07-01 14:46:09 +0200

    chore: update environment

--- packages/playground/src/vite.ts
+++ packages/playground/src/vite.ts
@@ -1,9 +1,7 @@
 import './assets/vite.css'
-import Minze from 'minze'
-import * as Elements from './module'
 import template from './template'
-
-Minze.defineAll(Elements)
+import { modules, defineAll } from './module'
+defineAll(modules)
 
 const app = document.querySelector<HTMLDivElement>('#app') ?? null
 if (app) app.innerHTML = template
