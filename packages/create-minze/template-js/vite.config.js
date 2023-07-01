c8c4f756b95a7aec20f3e091ac6938069c5735e2
Sergej Samsonenko <contact@sergej.codes>
2023-07-01 20:47:14 +0200

    chore: remove comments

--- packages/create-minze/template-js/vite.config.js
+++ packages/create-minze/template-js/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import minze from '@minzejs/vite-plugin-minze'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [minze({ entry: { module: 'src/module.js', cdn: 'src/cdn.js' } })]
 })
--- packages/create-minze/template-ts/vite.config.ts
+++ packages/create-minze/template-ts/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import minze from '@minzejs/vite-plugin-minze'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     minze({ entry: { module: 'src/module.ts', cdn: 'src/cdn.ts' } }),
