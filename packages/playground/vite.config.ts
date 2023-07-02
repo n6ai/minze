0e5daf09c2456859577a8252e1ca4acecd7d1d36
Sergej Samsonenko <contact@sergej.codes>
2023-07-02 17:27:12 +0200

    chore: update ignore patterns

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -5,6 +5,6 @@ import dts from 'vite-plugin-dts'
 export default defineConfig({
   plugins: [
     minze({ entry: { module: 'src/module.ts', cdn: 'src/cdn.ts' } }),
-    dts({ include: ['src/module.ts', 'src/lib/**/*.ts'] })
+    dts({ include: ['src/module.ts', 'src/lib/**/!(*.spec|*.stories).ts'] })
   ]
 })
