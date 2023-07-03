9f2f7c98db621704e4a67b352fece3f8ba64ad15
Sergej Samsonenko <contact@sergej.codes>
2023-07-04 01:29:43 +0200

    chore: update cli vite templates

--- packages/create-minze/template-vite-ts/src/vite.ts
+++ packages/create-minze/template-vite-ts/src/vite.ts
@@ -1,6 +1,6 @@
 import './assets/vite.css'
+import template from './template.html?raw'
 import { modules, defineAll } from './module'
-import template from './template'
 defineAll(modules)
 
 const app = document.querySelector<HTMLDivElement>('#app') ?? null
