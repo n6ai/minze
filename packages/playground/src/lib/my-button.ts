d5fd1bbf8b73f77bd36bf7f1ac56d3f2b5c47c04
Sergej Samsonenko <contact@sergej.codes>
2023-07-04 18:42:30 +0200

    chore: update playground template

---
+++ packages/playground/src/lib/my-button.ts
@@ -0,0 +1,41 @@
+import { MinzeElement } from 'minze'
+
+export class MyButton extends MinzeElement {
+  html = () => `
+    <button>
+      <slot></slot>
+    </button>
+  `
+
+  css = () => `
+    button {
+      display: inline-block;
+      border-radius: 8px;
+      border: 1px solid transparent;
+      padding: 0.6em 1.2em;
+      font-size: 1em;
+      font-weight: 500;
+      font-family: inherit;
+      background-color: rgb(249 249 249);
+      cursor: pointer;
+      user-select: none;
+      position: relative;
+      transition: border-color 0.25s;
+    }
+
+    button:hover {
+      border-color: var(--color-primary, rgb(55 245 220));
+    }
+
+    button:active {
+      outline: 3px solid var(--color-primary, rgb(55 245 220));
+      outline-offset: -2px;
+    }
+
+    @media (prefers-color-scheme: dark) {
+      button {
+        background-color: rgb(26 26 26);
+      }
+    }
+  `
+}
--- packages/playground/src/lib/playground-element.ts
+++
@@ -1,7 +0,0 @@
-import { MinzeElement } from 'minze'
-
-export class PlaygroundElement extends MinzeElement {
-  html = () => `
-    <div>Hello Minze!</div>
-  `
-}
