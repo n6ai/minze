6682947c79dcf5d0c5f45fa25ab5370e6c7dc23a
Sergej Samsonenko <contact@sergej.codes>
2023-07-26 20:21:44 +0200

    chore: fix @events test

--- packages/tests/e2e/minze-element/src/lib/minze-@events.ts
+++ packages/tests/e2e/minze-element/src/lib/minze-@events.ts
@@ -1,7 +1,12 @@
+import type { Reactive } from 'minze'
 import { MinzeElement } from 'minze'
 
+export interface MinzeAtEvents {
+  text: string
+}
+
 export class MinzeAtEvents extends MinzeElement {
-  text = 'not-clicked'
+  reactive: Reactive = [['text', 'not-clicked']]
 
   changeText = () => (this.text = 'clicked')
 
