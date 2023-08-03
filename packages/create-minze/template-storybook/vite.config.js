9a03ac8d70a3b17a5d9b7759b022088d2fa8ddf2
Sergej Samsonenko <contact@sergej.codes>
2023-08-03 17:38:12 +0200

    chore: update cli tempaltes

--- packages/create-minze/template-storybook/vite.config.js
+++ packages/create-minze/template-storybook/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'url'
 import minze from '@minzejs/vite-plugin-minze'
 
 export default defineConfig({
+  resolve: {
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  },
   plugins: [minze()]
 })
