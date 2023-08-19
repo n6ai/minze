445a193229bf31e4a27e3eba1e5d763db10a3012
Sergej Samsonenko <contact@sergej.codes>
2023-08-19 14:32:49 +0200

    chore: remove redundant define

--- packages/tests/e2e/minze-element/src/vite.ts
+++ packages/tests/e2e/minze-element/src/vite.ts
@@ -2,7 +2,3 @@ import './assets/vite.css'
 
 import { modules, defineAll } from './main'
 defineAll(modules)
-
-// component define method
-import { MinzeDefine } from './lib/minze-define'
-MinzeDefine.define()
