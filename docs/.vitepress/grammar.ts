4d65f8c2b3e0a2dda73c1b2ae0be7d3a32c81dd7
Sergej Samsonenko <contact@sergej.codes>
2024-04-22 21:13:11 +0200

    chore: update vitepress and grammar loading

---
+++ docs/.vitepress/grammar.ts
@@ -0,0 +1,15 @@
+import type { LanguageInput } from 'shiki'
+import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+
+const filePath = '../../packages/minze-vscode/syntaxes/minze.tmLanguage.json'
+const grammar = JSON.parse(
+  await readFile(fileURLToPath(new URL(filePath, import.meta.url)), 'utf-8')
+)
+
+export const languages: LanguageInput[] = [
+  {
+    injectTo: ['source.js', 'source.ts'],
+    ...grammar
+  }
+]
